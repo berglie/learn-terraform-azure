9cc4f9da0136fdbc46948f7c25c2f9cd999bf124
sblanglie <berglie.stian@gmail.com>
2022-08-25 20:38:04 +0200

    Add tags to resource group

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "West Europe"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
 }
 
 # Create a virtual network
